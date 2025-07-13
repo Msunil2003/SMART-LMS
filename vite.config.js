95d7637cf3b822360636408a1921a7451c5d091d
MSUNIL2003 <114647439+Msunil2003@users.noreply.github.com>
2025-07-14 01:19:34 +0530

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,7 @@
-// vite.config.js
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 5173,
-    allowedHosts: ['smart-lms.loca.lt'] // ✅ your custom LocalTunnel subdomain
-  }
 })
